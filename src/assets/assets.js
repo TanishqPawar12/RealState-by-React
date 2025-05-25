baa357b24ff61f4e1175646dedbc39532e5bb2ae
Tanishq Pawar <tanishqpawar@example.com>
2025-05-25 17:12:34 +0530

    Initial commit

---
+++ src/assets/assets.js
@@ -0,0 +1,101 @@
+import logo from './logo.svg'
+import logo_dark from './logo_dark.svg'
+import cross_icon from './cross_icon.svg'
+import menu_icon from './menu_icon.svg'
+import star_icon from './star_icon.svg'
+import left_arrow from './left_arrow.svg'
+import right_arrow from './right_arrow.svg'
+import header_img from './header_img.png'
+import brand_img from './brand_img.png'
+import project_img_1 from './project_img_1.jpg'
+import project_img_2 from './project_img_2.jpg'
+import project_img_3 from './project_img_3.jpg'
+import project_img_4 from './project_img_4.jpg'
+import project_img_5 from './project_img_5.jpg'
+import project_img_6 from './project_img_6.jpg'
+import profile_img_1 from './profile_img_1.png'
+import profile_img_2 from './profile_img_2.png'
+import profile_img_3 from './profile_img_3.png'
+
+export const assets = {
+    logo,
+    logo_dark,
+    cross_icon,
+    menu_icon,
+    star_icon,
+    header_img,
+    brand_img,
+    project_img_1,
+    project_img_2,
+    project_img_3,
+    project_img_4,
+    left_arrow,
+    right_arrow,
+}
+
+export const projectsData = [
+    {
+      title: "Skyline Haven",
+      price: "$2,50,000",
+      location: "California",
+      image: project_img_1
+    },
+    {
+      title: "Vista Verde",
+      price: "$2,50,000",
+      location: "San Francisco",
+      image: project_img_2
+    },
+    {
+      title: "Serenity Suites",
+      price: "$2,50,000",
+      location: "Chicago",
+      image: project_img_3
+    },
+    {
+      title: "Central Square",
+      price: "$2,50,000",
+      location: "Los Angeles",
+      image: project_img_4
+    },
+    {
+      title: "Vista Verde",
+      price: "$2,50,000",
+      location: "San Francisco",
+      image: project_img_5
+    },
+    {
+      title: "Serenity Suites",
+      price: "$2,50,000",
+      location: "Chicago",
+      image: project_img_6
+    },
+    
+  ];
+
+  export const testimonialsData = [
+    {
+        name: "Tanishq Pawar",
+        title: "Founder",
+        image: profile_img_1,
+        alt: "Portrait of Donald Jackman",
+        rating: 5,
+        text: "Their team brought my vision to life from day one. As a founder, I wanted each property to reflect our core values—and they delivered exactly that. Their dedication and precision are truly admirable."
+    },
+    {
+        name: "Shamoil Rao",
+        title: "UI/UX Designer",
+        image: profile_img_3,
+        alt: "Portrait of Richard Nelson",
+        rating: 4,
+        text: "Working with them was seamless. Every design input I shared was carefully considered, and the final outcome blended aesthetics with functionality. Their collaborative spirit and eye for detail made the experience exceptional"
+    },
+    {
+        name: "Simarjeet Singh Arora",
+        title: "Co-Founder",
+        image: profile_img_2,
+        alt: "Portrait of James Washington",
+        rating: 5,
+        text: "As a co-founder, I needed a team that could match my passion and standards. They exceeded expectations by turning ideas into impactful spaces. Their professionalism and commitment were evident in every interaction."
+    }
+];
